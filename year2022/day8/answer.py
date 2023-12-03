b91408a76e8fdc70bca18896ad73a901edc01140
3LexW <kezKQF365FC3Eg3ZHQNZK9xG>
2023-12-03 21:54:20 +0800

    Complete year 2022 day 8 puzzle 2

--- year2022/day8/answer.py
+++ year2022/day8/answer.py
@@ -1,48 +1,58 @@
 import os
 from typing import Set, Tuple
+from functools import reduce
 
 with open(f"{os.path.dirname(__file__)}/input.txt") as f:
-    lines = [l.strip() for l in f.readlines()]
+    grid = [l.strip() for l in f.readlines()]
 
-total_rows = len(lines)
-total_columns = len(lines[0])
+total_rows = len(grid)
+total_columns = len(grid[0])
 
-valid: Set[Tuple[int, int]]= set()
+valid: Set[Tuple[int, int]] = set()
+
 
 def edge_check(row, column, total_rows, total_columns):
-    return True if row == 0 or row == total_rows - 1 or column == 0 or column == total_columns - 1 else False
+    return (
+        True
+        if row == 0
+        or row == total_rows - 1
+        or column == 0
+        or column == total_columns - 1
+        else False
+    )
+
 
 for row in range(0, total_rows):
     # Get the edge values
     for column in range(0, total_columns):
         if edge_check(row, column, total_rows, total_columns):
-            valid.add((row, column)) 
+            valid.add((row, column))
             continue
 
-    line = lines[row]
+    line = grid[row]
 
     # Scan from left to right, if valid, add to valid list
     row_max = 0
     for column in range(0, total_columns):
-        value = int(lines[row][column])
+        value = int(grid[row][column])
         if value > row_max:
             valid.add((row, column))
         row_max = max(row_max, value)
-    
+
     # Scan from right to left
     row_max = 0
     for column in range(total_columns - 1, -1, -1):
-        value = int(lines[row][column])
+        value = int(grid[row][column])
         if value > row_max:
             valid.add((row, column))
         row_max = max(row_max, value)
 
 for column in range(0, total_columns):
-    line = ''.join([row[column] for row in lines])
+    line = "".join([row[column] for row in grid])
     # Scan from top to bottom
     column_max = 0
     for row in range(0, total_rows):
-        value = int(lines[row][column])
+        value = int(grid[row][column])
         if value > column_max:
             valid.add((row, column))
         column_max = max(column_max, value)
@@ -50,10 +60,37 @@ for column in range(0, total_columns):
     # Scan from bottom to top
     column_max = 0
     for row in range(total_rows - 1, -1, -1):
-        value = int(lines[row][column])
+        value = int(grid[row][column])
         if value > column_max:
             valid.add((row, column))
         column_max = max(column_max, value)
 
 
 print(f"Puzzle 1: {len(valid)}")
+
+
+# Puzzle 2
+max_score = 0
+
+for row in range(0, total_rows):
+    for column in range(0, total_columns):
+        value = grid[row][column]
+        if edge_check(row, column, total_rows, total_columns):
+            score = 0
+        else:
+            left, right, top, bottom = 1, 1, 1, 1
+            while column - left > 0 and grid[row][column - left] < value:
+                left += 1
+            while (
+                column + right < total_columns - 1 and grid[row][column + right] < value
+            ):
+                right += 1
+            while row - top > 0 and grid[row - top][column] < value:
+                top += 1
+            while row + bottom < total_rows - 1 and grid[row + bottom][column] < value:
+                bottom += 1
+            score = reduce(lambda a, b: a * b, list([left, right, top, bottom]))
+
+        max_score = max(max_score, score)
+
+print(f"Puzzle 2: {max_score}")
