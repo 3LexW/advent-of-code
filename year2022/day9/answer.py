f3cf64482f585830fc692ee4716aa7baff417dc4
3LexW <kezKQF365FC3Eg3ZHQNZK9xG>
2023-12-04 00:22:51 +0800

    Complete year 2022 day 9 puzzle 2

--- year2022/day9/answer.py
+++ year2022/day9/answer.py
@@ -1,28 +1,53 @@
 import os
-from typing import Set, Tuple
+from typing import Dict, Set, Tuple
 
-# Set beginning position
-H = (0, 0)
-T = (0, 0)
+mapping = {"L": (-1, 0), "R": (1, 0), "U": (0, 1), "D": (0, -1)}
 
-T_history: Set[Tuple[int, int]] = set([T])
-H_previous_step = (0, 0)
 
-mapping = {
-    'L': (-1, 0),
-    'R': (1, 0),
-    'U': (0, 1),
-    'D': (0, -1)
-}
+def move_tail(head: Tuple[int, int], tail: Tuple[int, int]) -> Tuple[int, int]:
+    difference = (head[0] - tail[0], head[1] - tail[1])
+    shift_map: Dict[Tuple[int, int], Tuple[int, int]] = {
+        (2, 1): (1, 1),
+        (1, 2): (1, 1),
+        (2, 0): (1, 0),
+        (2, -1): (1, -1),
+        (1, -2): (1, -1),
+        (0, -2): (0, -1),
+        (-1, -2): (-1, -1),
+        (-2, -1): (-1, -1),
+        (-2, 0): (-1, 0),
+        (-2, 1): (-1, 1),
+        (-1, 2): (-1, 1),
+        (0, 2): (0, 1),
+        (2, 2): (1, 1),
+        (-2, -2): (-1, -1),
+        (-2, 2): (-1, 1),
+        (2, -2): (1, -1),
+    }
 
-with open(f"{os.path.dirname(__file__)}/input.txt") as f:
-    for line in f:
-        direction, step = line.strip().split(' ')
-        for _ in range(0, int(step)):
-            H_previous_step = H
-            H = (H[0] + mapping[direction][0], H[1] + mapping[direction][1])
-            if abs(T[0] - H[0]) >= 2 or abs(T[1] - H[1]) >= 2:
-                T = H_previous_step
-                T_history.add(T)
+    shift = shift_map.get(difference)
+    return (tail[0] + shift[0], tail[1] + shift[1]) if shift else tail
 
-print(f"Puzzle 1: {len(T_history)}")
+
+def get_ans(tail_count: int) -> int:
+    T_history: Set[Tuple[int, int]] = set([(0, 0)])
+
+    # First knot is the head, and last knot is the tail
+    knots = [(0, 0)] + [(0, 0) for _ in range(0, tail_count)]
+    with open(f"{os.path.dirname(__file__)}/input.txt") as f:
+        for line in f:
+            direction, step = line.strip().split(" ")
+            for _ in range(0, int(step)):
+                knots[0] = (
+                    knots[0][0] + mapping[direction][0],
+                    knots[0][1] + mapping[direction][1],
+                )
+                for i in range(1, len(knots)):
+                    knots[i] = move_tail(knots[i - 1], knots[i])
+                T_history.add(knots[-1])
+
+    return len(T_history)
+
+
+print(f"Puzzle 1: {get_ans(1)}")
+print(f"Puzzle 2: {get_ans(9)}")
