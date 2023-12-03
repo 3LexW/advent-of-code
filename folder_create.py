538027473af70eff9783d9958cbde757e7543d4e
3LexW <kezKQF365FC3Eg3ZHQNZK9xG>
2023-12-03 16:21:13 +0800

    Only single input file, change create folder structure

--- folder_create.py
+++ folder_create.py
@@ -3,16 +3,16 @@ import os
 # Get user input for year, day, and puzzle
 year = input("Enter the year: ")
 day = input("Enter the day: ")
-puzzle = input("Enter the puzzle number: ")
 
 # Create the folder structure
-folder_path = f"year{year}/day{day}/puzzle{puzzle}"
+folder_path = f"year{year}/day{day}"
 os.makedirs(folder_path, exist_ok=True)
 
 # Create the files
 answer_file = os.path.join(folder_path, "answer.py")
 input_file = os.path.join(folder_path, "input.txt")
-puzzle_file = os.path.join(folder_path, "puzzle.md")
+puzzle_file_1 = os.path.join(folder_path, "puzzle1.md")
+puzzle_file_2 = os.path.join(folder_path, "puzzle2.md")
 
 # Create the answer.py file
 with open(answer_file, "w") as file:
@@ -23,6 +23,10 @@ with open(answer_file, "w") as file:
 with open(input_file, "w") as file:
     file.write("# Add your input here")
 
-# Create the puzzle.md file
-with open(puzzle_file, "w") as file:
+# Create the puzzle1.md file
+with open(puzzle_file_1, "w") as file:
     file.write("# Add the puzzle description here")
+
+# Create the puzzle2.md file
+with open(puzzle_file_2, "w") as file:
+    file.write("# Add the second puzzle description here")
