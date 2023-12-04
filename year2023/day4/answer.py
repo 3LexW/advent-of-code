b1db26289f3efca180ce771b1e2f60c3120ecd28
Felix Wong <pakywong9@gmail.com>
2023-12-04 05:57:49 +0000

    Complete year 2023 day 4 puzzle 2

--- year2023/day4/answer.py
+++ year2023/day4/answer.py
@@ -4,13 +4,21 @@ import re
 points = 0
 
 with open(f"{os.path.dirname(__file__)}/input.txt") as f:
-    pattern = r"Card.*(\d+): ([\d ]+) \| ([\d ]+)"
-    for line in f:
+    lines = f.readlines()
+    cards = [1 for _ in range(0, len(lines))]
+    pattern = r"Card +(\d+): ([\d ]+) \| ([\d ]+)"
+    for line in lines:
         card, winning, hand = re.match(pattern, line.strip()).groups()
+
+        card = int(card)
         winning = re.findall(r"(\d+)", winning)
         hand = re.findall(r"(\d+)", hand)
 
         match_count = len([x for x in hand if x in winning])
+        for shift in range(0, match_count):
+            if card + shift < len(cards):
+                cards[card + shift] += cards[card - 1]
+
         match match_count:
             case 0:
                 pass
@@ -20,3 +28,4 @@ with open(f"{os.path.dirname(__file__)}/input.txt") as f:
                 points += 2 ** (match_count - 1)
 
 print(f'Puzzle 1: {points}')
+print(f'Puzzle 2: {sum(cards)}')
