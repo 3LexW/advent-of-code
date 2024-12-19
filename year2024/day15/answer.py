b60fab4761b6421aaacc1ea6afe51e7adec5a690
Felix Wong <FELIX.WONG2@hthk.com>
2024-12-19 14:04:46 +0900

    Complete year 2024 day 15

--- year2024/day15/answer.py
+++ year2024/day15/answer.py
@@ -10,13 +10,35 @@ class Warehouse:
     command_pointer: int
     robot_pos: Tuple[int, int]
 
-    def __init__(self, map, command):
+    def __init__(self, map, command, expand=False):
         self.map = map
         self.command = command
+        if expand:
+            self.map = self.get_expand_map()
         robot_row = [i for i, x in enumerate(self.map) if "@" in x][0]
         robot_col = [j for j, row in enumerate(self.map[robot_row]) if "@" in row][0]
         self.robot_pos = (robot_row, robot_col)
         self.command_pointer = 0
+
+    def _get_extended_tile(_, tile):
+        match tile:
+            case "#":
+                return ["#", "#"]
+            case "O":
+                return ["[", "]"]
+            case ".":
+                return [".", "."]
+            case "@":
+                return ["@", "."]
+
+    def get_expand_map(self):
+        expanded_map = []
+        for row in self.map:
+            new_row = []
+            for col in row:
+                new_row.extend(self._get_extended_tile(col))
+            expanded_map.append(new_row)
+        return expanded_map
 
     def _get_current_command(self):
         return self.command[self.command_pointer]
@@ -39,6 +61,8 @@ class Warehouse:
 
     def execute_move(self):
         robot_row, robot_col = self.robot_pos
+
+        current_move = self._get_current_command()
         move_row, move_col = self._get_move()
         self.command_pointer += 1
 
@@ -47,6 +71,10 @@ class Warehouse:
 
         while len(stack) > 0:
             row, col, next_val = stack.pop()
+
+            if (row, col) in move_to_do.keys() and move_to_do[(row, col)] != ".":
+                continue  # Visited
+
             if self.map[row][col] == "#":
                 return  # Do nothing
 
@@ -54,9 +82,21 @@ class Warehouse:
                 move_to_do[(row, col)] = next_val
                 continue  # Valid
 
-            if self.map[row][col] == "O":  # Puzzle 1
+            if self.map[row][col] == "O" or self.map[row][col] in "[]":  # Puzzle 1
                 move_to_do[(row, col)] = next_val
                 stack.append((row + move_row, col + move_col, self.map[row][col]))
+
+            if self.map[row][col] in "[]" and current_move in "^v":
+                match self.map[row][col]:
+                    case "[":  # Check right
+                        stack.append((row, col + 1, "."))
+                    case "]":
+                        stack.append((row, col - 1, "."))
+                    case _:
+                        self.print_map()
+                        raise ValueError(
+                            "It is not a legit warehouse map since box pattern is not good"
+                        )
 
         # Move the points if valid
         for (row, col), next_val in move_to_do.items():
@@ -72,7 +112,7 @@ class Warehouse:
         total_score = 0
         for row in range(len(self.map)):
             for col in range(len(self.map[0])):
-                if self.map[row][col] == "O":
+                if self.map[row][col] in "O[":
                     total_score += 100 * row + col
         return total_score
 
@@ -85,6 +125,12 @@ with open(f"{os.path.dirname(__file__)}/input.txt") as f:
     map = [list(x) for x in data if "#" in x]
     command = list("".join([x for x in data if "#" not in x and len(x) > 0]))
 
-warehouse = Warehouse(deepcopy(map), command)
+warehouse = Warehouse(deepcopy(map), command, False)
 warehouse.complete_command()
-print(f"Puzzle 1: {warehouse.get_gps_score()}")
+warehouse.print_map()
+print(f"Puzzle 1: {warehouse.get_gps_score()}\n")
+
+warehouse = Warehouse(deepcopy(map), command, True)
+warehouse.complete_command()
+warehouse.print_map()
+print(f"Puzzle 2: {warehouse.get_gps_score()}\n")
