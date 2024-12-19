ba90663ddb8e8a95fca659d6e8130e177c23e35d
Felix Wong <FELIX.WONG2@hthk.com>
2024-12-19 12:54:51 +0900

    Update puzzle 1

--- year2024/day15/answer.py
+++ year2024/day15/answer.py
@@ -1,6 +1,7 @@
 import os
 from pprint import pp
 from typing import List, Tuple
+from copy import deepcopy
 
 
 class Warehouse:
@@ -17,8 +18,11 @@ class Warehouse:
         self.robot_pos = (robot_row, robot_col)
         self.command_pointer = 0
 
+    def _get_current_command(self):
+        return self.command[self.command_pointer]
+
     def _get_move(self):
-        match self.command[self.command_pointer]:
+        match self._get_current_command():
             case "^":
                 move_row, move_col = (-1, 0)
             case "<":
@@ -36,31 +40,44 @@ class Warehouse:
     def execute_move(self):
         robot_row, robot_col = self.robot_pos
         move_row, move_col = self._get_move()
-        shift = 1
-        while self.map[robot_row + move_row * shift][
-            robot_col + move_col * shift
-        ] not in (".", "#"):
-            shift += 1
-        if self.map[robot_row + move_row * shift][robot_col + move_col * shift] == ".":
-            # Move all the items
-            for i in range(0, shift):
-                self.map[robot_row + move_row * (shift - i)][robot_col + move_col * (shift - i)] = self.map[robot_row + move_row * (shift - i - 1)][robot_col + move_col * (shift - i - 1)]
-            self.map[robot_row][robot_col] = "."
-            self.robot_pos = (robot_row + move_row, robot_col + move_col)
         self.command_pointer += 1
-    
+
+        stack = [(robot_row + move_row, robot_col + move_col, "@")]
+        move_to_do = {(robot_row, robot_col): "."}
+
+        while len(stack) > 0:
+            row, col, next_val = stack.pop()
+            if self.map[row][col] == "#":
+                return  # Do nothing
+
+            if self.map[row][col] == ".":
+                move_to_do[(row, col)] = next_val
+                continue  # Valid
+
+            if self.map[row][col] == "O":  # Puzzle 1
+                move_to_do[(row, col)] = next_val
+                stack.append((row + move_row, col + move_col, self.map[row][col]))
+
+        # Move the points if valid
+        for (row, col), next_val in move_to_do.items():
+            self.map[row][col] = next_val
+        self.robot_pos = (robot_row + move_row, robot_col + move_col)
+
     def complete_command(self):
         while self.command_pointer < len(self.command):
             self.execute_move()
+            # self.print_map()
 
     def get_gps_score(self):
         total_score = 0
         for row in range(len(self.map)):
             for col in range(len(self.map[0])):
                 if self.map[row][col] == "O":
-                    total_score += (100 * row + col)
+                    total_score += 100 * row + col
         return total_score
 
+    def print_map(self):
+        print("\n".join(["".join(row) for row in self.map]))
 
 
 with open(f"{os.path.dirname(__file__)}/input.txt") as f:
@@ -68,6 +85,6 @@ with open(f"{os.path.dirname(__file__)}/input.txt") as f:
     map = [list(x) for x in data if "#" in x]
     command = list("".join([x for x in data if "#" not in x and len(x) > 0]))
 
-warehouse = Warehouse(map, command)
+warehouse = Warehouse(deepcopy(map), command)
 warehouse.complete_command()
 print(f"Puzzle 1: {warehouse.get_gps_score()}")
