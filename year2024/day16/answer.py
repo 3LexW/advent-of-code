6da99f65fa8176a336287a559bf2bd0b23a67454
Felix Wong <FELIX.WONG2@hthk.com>
2024-12-24 08:14:06 +0900

    Complete year 2024 day 16

--- year2024/day16/answer.py
+++ year2024/day16/answer.py
@@ -1,67 +1,116 @@
 import os
 from typing import List, Tuple
+from math import inf
+import heapq
 
 
 class Map:
     graph: List[List[str]]
     starting_point: Tuple[int, int]
     ending_point: Tuple[int, int]
+    ending_state: Tuple[int, int, int] = (None, None, None)
+
+    queue = []  # (score, row, col, facing)
+    lowest_score = {}
+    backtrack = {}
+    valid_backtrace_paths = []
 
     def __init__(self, graph):
         self.graph = graph
         self.starting_point = self._get_point("S")
         self.ending_point = self._get_point("E")
 
+        self.queue.append(
+            (0, self.starting_point[0], self.starting_point[1], 0, None, None, None)
+        )
+        self._dijkstra()
+
+        self.queue.append(self.ending_state)
+        self._trace()
+
     def _get_point(self, target: str):
         row = [i for i, row in enumerate(self.graph) if target in row][0]
         col = self.graph[row].index(target)
         return row, col
 
-    def dijkstra(self):
-        visited = {}
-        scores = {}
+    def _dijkstra(self):
         directions = [(0, 1), (-1, 0), (0, -1), (1, 0)]
 
-        def add_to_queue(row, col, facing, score):
-            scores[(row, col, facing)] = score
-
-        def pop_from_queue():
-            entry = min(scores.items(), key=lambda item: item[1])
-            scores.pop(entry[0])
-            return entry[0], entry[1]
-
-        add_to_queue(self.starting_point[0], self.starting_point[1], 0, 0)
-        while len(scores) > 0:
-            (row, col, facing), score = pop_from_queue()
-
-            if (row, col, facing) in visited:
-                continue  # The point is already optimized
+        while len(self.queue) > 0:
+            (score, row, col, facing, last_row, last_col, last_facing) = heapq.heappop(
+                self.queue
+            )
 
             if self.graph[row][col] == "#":
                 continue  # Out of bound
 
-            if (row, col) == self.ending_point:
-                return score
+            if score > self.lowest_score.get((row, col, facing), inf):
+                continue  # Not optimal
 
-            visited[(row, col, facing)] = score
+            if score < self.lowest_score.get((row, col, facing), inf):
+                self.lowest_score[(row, col, facing)] = score
+                self.backtrack[(row, col, facing)] = set()
+            self.backtrack[(row, col, facing)].add((last_row, last_col, last_facing))
+
+            if (row, col) == self.ending_point:
+                if score < self.lowest_score.get(self.ending_state, inf):
+                    self.ending_state = (row, col, facing)
 
             # go straight
             shift_row, shift_col = directions[facing]
-            add_to_queue(row + shift_row, col + shift_col, facing, score + 1)
+            heapq.heappush(
+                self.queue,
+                (score + 1, row + shift_row, col + shift_col, facing, row, col, facing),
+            )
 
             # left 90
             shift_row, shift_col = directions[(facing - 1) % 4]
-            add_to_queue(
-                row + shift_row, col + shift_col, (facing - 1) % 4, score + 1001
+            heapq.heappush(
+                self.queue,
+                (
+                    score + 1001,
+                    row + shift_row,
+                    col + shift_col,
+                    (facing - 1) % 4,
+                    row,
+                    col,
+                    facing,
+                ),
             )
 
             # right 90
             shift_row, shift_col = directions[(facing + 1) % 4]
-            add_to_queue(
-                row + shift_row, col + shift_col, (facing + 1) % 4, score + 1001
+            heapq.heappush(
+                self.queue,
+                (
+                    score + 1001,
+                    row + shift_row,
+                    col + shift_col,
+                    (facing + 1) % 4,
+                    row,
+                    col,
+                    facing,
+                ),
             )
+
+    def _trace(self):
+        while len(self.queue) > 0:
+            current_node = self.queue.pop(0)
+            if current_node in self.valid_backtrace_paths or current_node == (
+                None,
+                None,
+                None,
+            ):
+                continue
+
+            self.valid_backtrace_paths.append(current_node)
+            for node in self.backtrack[current_node]:
+                self.queue.append(node)
 
 
 with open(f"{os.path.dirname(__file__)}/input.txt") as f:
     map = Map([list(x.strip()) for x in f.readlines()])
-    print(f"Puzzle 1: {map.dijkstra()}")
+    print(
+        f"Puzzle 1: {[score for (row, col, _), score in map.lowest_score.items() if (row, col) == map.ending_point][0]}"
+    )
+    print(f"PUzzle 2: {len(set((r, c) for (r, c, _) in map.valid_backtrace_paths))}")
