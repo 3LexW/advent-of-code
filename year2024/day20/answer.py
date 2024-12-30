4e97340591597d43f8607ae23128bf8b4198921e
Felix Wong <FELIX.WONG2@hthk.com>
2024-12-31 07:34:55 +0900

    Complete year 2024 day 20

--- year2024/day20/answer.py
+++ year2024/day20/answer.py
@@ -18,33 +18,51 @@ queue = [(0, start_row, start_col)]
 paths = []
 while queue:
     score, row, col = heapq.heappop(queue)
+    graph[row][col] = score
     paths.append((row, col))
     for dr, dc in shifts:
         if not 0 <= row + dr < height or not 0 <= col + dc < width:
             continue  # Not a valid point
-        if graph[row + dr][col + dc] == "#":
-            continue
-        if (row + dr, col + dc) in paths:
+        if graph[row + dr][col + dc] not in ["S", "E", "."]:
             continue
         heapq.heappush(queue, (score + 1, row + dr, col + dc))
 
 
-def get_cheat_paths(max_cheat):
-    ans = {}
-    for i in range(len(paths)):
-        (from_r, from_c) = paths[i]
-        for j in range(i + 1, len(paths)):
-            if j - i <= max_cheat:
-                continue  # You can just walk to it
-            (to_r, to_c) = paths[j]
-            distance = abs(to_r - from_r) + abs(to_c - from_c)
-            if distance <= max_cheat:
-                if j - i - max_cheat not in ans:
-                    ans[j - i - max_cheat] = 0
-                ans[j - i - max_cheat] = ans[j - i - max_cheat] + 1
-    return dict(sorted(ans.items()))
+def get_cheat_paths(max_cheat, threshold):
+    ans = 0
+    for from_r in range(height):
+        for from_c in range(width):
+            if graph[from_r][from_c] == "#":
+                continue
+            for radius in range(2, max_cheat + 1):
+                for shift_r in range(radius + 1):
+                    shift_c = radius - shift_r
+                    loop = [
+                        (from_r + shift_r, from_c + shift_c),
+                        (from_r + shift_r, from_c - shift_c),
+                        (from_r - shift_r, from_c + shift_c),
+                        (from_r - shift_r, from_c - shift_c),
+                    ]
+                    if shift_r == 0:
+                        loop.remove((from_r - shift_r, from_c + shift_c))
+                        loop.remove((from_r - shift_r, from_c - shift_c))
+                    if shift_c == 0:
+                        loop.remove((from_r + shift_r, from_c - shift_c))
+                        loop.remove((from_r - shift_r, from_c - shift_c))
+
+                    for to_r, to_c in loop:
+                        if not (0 <= to_r < height and 0 <= to_c < width):
+                            continue  # Out of bound
+                        if graph[to_r][to_c] == "#":
+                            continue  # Cheat failed
+                        time_saved = graph[to_r][to_c] - graph[from_r][from_c]
+                        if time_saved >= threshold + radius:
+                            ans += 1
+    return ans
 
 
-p1 = get_cheat_paths(2)
-print(p1)
-print(f"Puzzle 1: {sum([value for key, value in p1.items() if key >= 100])}")
+p1 = get_cheat_paths(2, 100)
+print(f"Puzzle 1: {p1}")
+
+p2 = get_cheat_paths(20, 100)
+print(f"Puzzle 2: {p2}")
